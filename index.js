e80f5263792e225c3a7fc19bd3a79ed21fb86896
Carlos Soto <mrlocked4@gmail.com>
2025-04-13 17:58:37 -0600

    se quito el html commit

--- index.js
+++ index.js
@@ -26,7 +26,7 @@ app.use(express.static(path.join(__dirname)));
 
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'qr.html'));
+    res.sendFile(path.join(__dirname, 'qr.png'));
 });
 
 
